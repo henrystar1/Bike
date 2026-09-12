b090bae5ec2992733318064ccd941a1c3eddc883
henrystar1 <henrymack@mein.gmx>
2026-09-12 23:32:20 +0200

    Delete CUBE-Bike-Builder directory

--- CUBE-Bike-Builder/start-server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title CUBE Nuroad Bike Builder 3D
-echo Starte lokalen Webserver auf http://localhost:8000 ...
-start "" http://localhost:8000
-python -m http.server 8000
--- CUBE-Bike-Builder/start-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Lokaler Webserver: http://localhost:8000  (Beenden mit Strg+C)"
-python3 -m http.server 8000
